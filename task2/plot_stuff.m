73001815b6f37747e6b228f84f6309e4ca04e47b
karlssonalbinka <karlsson.albin.ka@gmail.com>
2014-12-14 12:22:37 +0100

    df

--- task2/plot_stuff.m
+++ task2/plot_stuff.m
@@ -27,3 +27,25 @@ plot(avgTimes,'r')
 hold on
 plot(0.05*[1 2 4 8].^2.8)
 hold off
+
+%% Felsökning
+clear all
+clc
+clf
+close all
+
+data = dlmread('grid.data','\t');
+dataStart = dlmread('start.data','\t');
+
+data = data(:,1:end-1);
+dataStart = dataStart(:,1:end-1);
+
+nPoints = length(data);
+nPointsStart = length(dataStart);
+
+surf([0:nPoints-1],[0:nPoints-1],data)
+view(45,0)
+
+figure
+surf([0:nPointsStart-1],[0:nPointsStart-1],dataStart)
+view(45,0)
